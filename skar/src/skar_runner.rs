c919cba18d628b06745a7bdb62277e98b98f04b5
Özgür Akkurt <oezgurmakkurt@gmail.com>
2023-07-21 00:53:56 +0600

    fix: fix batch aggregation (#25)

--- skar/src/skar_runner.rs
+++ skar/src/skar_runner.rs
@@ -73,7 +73,7 @@ impl Write {
                         .chain(std::iter::once(blocks))
                         .collect(),
                     transactions: state
-                        .blocks
+                        .transactions
                         .iter()
                         .cloned()
                         .chain(std::iter::once(transactions))
